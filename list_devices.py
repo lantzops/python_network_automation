6eab24d7a3959b8d4ccce749f11a6a570fe7640e
lantzops <lantz.mitch@gmail.com>
2026-06-23 15:38:26 -0400

    complete list_devices.py version 2 plus screenshot

--- list_devices.py
+++ list_devices.py
@@ -1,10 +1,14 @@
 import csv
 
+count = 0
+print("Network Device List \n----------------------")
+
 with open("network_devices.csv") as file:
     reader = csv.DictReader(file)
-
     for row in reader:
         print(row["Device Name"])
+        count += 1
 
+print(f"Total Number of Devices = {count}")
 
 
